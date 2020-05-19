3cd2af340c0f4180e7885368b3a3b567d18a0784
Rohan Pawar <rohan.pawar@zensar.com>
2020-05-19 16:05:31 +0530

    changes

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -12,8 +13,10 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    // import HttpClientModule after BrowserModule.
+    HttpClientModule,
   ],
   declarations: [HomePage]
 })
-export class HomePageModule {}
+export class HomePageModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,80 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  private forecastData: any;
+  private errMsg: any;
+  private countryList: any;
+  private selectedCountry: string;
+  private stateList: any;
+  private selectedState: string;
+  private cityList: any;
+  private selectedCity: string;
 
+  constructor(private http: HttpClient) {
+    this.getCountry();
+  }
+
+
+
+  getCountry() {
+    let urlCountryList = 'https://api.airvisual.com/v2/countries?';
+    this.callForecastApi(urlCountryList).subscribe((data) => {
+      if (data) {
+        this.countryList = data.data
+      }
+    }, (error) => {
+      console.log(error, 'subs')
+      this.errMsg = error
+    });
+  }
+
+  selectCountry() {
+    console.log(this.selectedCountry)
+    let urlStateList = `https://api.airvisual.com/v2/states?country=${this.selectedCountry}`;
+    this.callForecastApi(urlStateList).subscribe((data) => {
+      if (data) {
+        this.stateList = data.data
+      }
+    }, (error) => {
+      console.log(error, 'subs')
+      this.errMsg = error
+    });
+  }
+
+  selectState() {
+    console.log(this.selectedState)
+    let urlStateList = `https://api.airvisual.com/v2/cities?state=${this.selectedState}&country=${this.selectedCountry}`;
+    this.callForecastApi(urlStateList).subscribe((data) => {
+      if (data) {
+        this.cityList = data.data
+      }
+    }, (error) => {
+      console.log(error, 'subs')
+      this.errMsg = error
+    });
+  }
+
+  getforecastData() {
+    let urlCityWeather = `https://api.airvisual.com/v2/city?city=${this.selectedCity}&state=${this.selectedState}&country=${this.selectedCountry}`;
+    this.callForecastApi(urlCityWeather).subscribe((data) => {
+      if (data) {
+        this.forecastData = data.data
+        console.log(this.forecastData)
+      }
+    }, (error) => {
+      console.log(error, 'subs')
+      this.errMsg = error
+    });
+  }
+
+  callForecastApi(url): Observable<any> {
+    return this.http.get(`${url}&key=83d60b33-fc09-4fbd-a5ab-1d4cd7e2e76b`)
+      .pipe(catchError(res => this.handleError(res)))
+  }
+
+  handleError(error: Response | any) {
+    let errorFromServer = error.error.data.message;
+    return throwError(errorFromServer);
+  }
 }
+
